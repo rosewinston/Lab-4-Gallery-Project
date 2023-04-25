2f4965a34a673ce709de17f619d195f003f17882
rosewinston <winston1@kenyon.edu>
2023-04-25 18:39:02 -0400

    made gallery feedback prettier

--- gallery.js
+++ gallery.js
@@ -7,6 +7,21 @@ var inthandle;
 var inthandle2;
 var color = '#343a40';
 
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]")
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null) return
+
+    let currentDropdown
+    if (isDropdownButton) {
+        currentDropdown = e.target.closest('[data-dropdown]')
+        currentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === currentDropdown) return
+        dropdown.classList.remove('active')
+    })
+})
 
 document.addEventListener("DOMContentLoaded", function() {
     setColor(color);
