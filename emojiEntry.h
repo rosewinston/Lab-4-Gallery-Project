78b2c40ad97ab7e0118590c04cf69f1411409902
michpolak <124084861+michpolak@users.noreply.github.com>
2023-04-28 16:01:35 -0400

    updated emojiEntry.h

--- emojiEntry.h
+++ emojiEntry.h
@@ -1,1 +1,24 @@
+#include <mariadb/conncpp.hpp>
+#include <string>
+using namespace std;
 
+#ifndef EMOJIENTRY_H
+#define EMOJIENTRY_H
+
+class emojiEntry {
+public:
+     colorEntry();
+     colorEntry(sql::SQLString id, sql::SQLString timestamp, sql::SQLString art_piece, sql::SQLString color);
+     string text();
+     string json();
+     
+  string id;
+  string timestamp;
+  string art_piece;
+  string color;
+
+private:
+
+};
+
+#endif /* EMOJIENTRY_H */
