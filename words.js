4f2d9c28ba68c04ba9b5d577d425185a0716a449
michpolak <124084861+michpolak@users.noreply.github.com>
2023-05-01 19:46:34 -0400

    added formatWords and fetchWords

--- words.js
+++ words.js
@@ -1,6 +1,46 @@
-window.onload = function(argument) {
+var baseUrl = 'http://18.222.189.198:5005';
 
-	var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
+window.onload = function(argument) {
+	
+	function fetchWords() {
+		// default timestamp, change later
+		timestamp = "2023-05-01";
+   		fetch(baseUrl + '/retrieve/words/'+timestamp, {
+   		method: 'get'
+   		})
+   		.then(response => response.json())
+   		.then(data => formatWords(data))
+   		.catch(error => {
+   	 	{alert("Error: Something went wrong:" + error); }
+  	 	})
+   	}
+	
+   	function formatWords(data){
+		var result = data;
+		var formattedString = "";
+		var first = true; 
+	 	if (result["status"]=="success"){
+			var wordList = result["words"]; 
+			wordList.forEach(function (word) {
+				if (first){
+					formattedString+=word; 
+					first = false; 
+				}else{
+					formattedString+=" " + word; 
+				}
+    			});
+			return formattedString; 
+		}else{
+			alert("Error: word cloud retrieve failed, possibly no words"); 
+		}
+	}
+		
+
+
+
+
+	var wordsRetrieved = fetchWords(); 
+	//var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
 	var words = {};
 	var words_attr = [];
 	string_handle(moby);
