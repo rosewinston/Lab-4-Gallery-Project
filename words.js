09df0d6433f062b7f0f56ca9d5912c673642e432
michpolak <124084861+michpolak@users.noreply.github.com>
2023-05-02 21:57:02 -0400

    oops!

--- words.js
+++ words.js
@@ -1,35 +1,42 @@
 var baseUrl = 'http://18.222.189.198:5005';
 
 window.onload = function(argument) {
+	var words = {};
+	var wordsRetrieved = fetchWords(words); 
+	//var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
+	var words_attr = [];
+	string_handle(wordsRetrieved);
 	
-	function fetchWords() {
+	
+	function fetchWords(words) {
 		// default timestamp, change later
 		timestamp = "2023-05-01";
    		fetch(baseUrl + '/retrieve/words/'+timestamp, {
    		method: 'get'
    		})
    		.then(response => response.json())
-   		.then(data => formatWords(data))
+   		.then(data => formatWords(words, data))
    		.catch(error => {
    	 	{alert("Error: Something went wrong:" + error); }
   	 	})
    	}
 	
-   	function formatWords(data){
+   	function formatWords(words, data){
 		var result = data;
 		var formattedString = "";
 		var first = true; 
 	 	if (result["status"]=="success"){
 			var wordList = result["words"]; 
 			wordList.forEach(function (word) {
-				if (first){
-					formattedString+=word; 
-					first = false; 
-				}else{
-					formattedString+=" " + word; 
-				}
+				//if (first){
+					//formattedString+=word; 
+					//first = false; 
+				//}else{
+					//formattedString+=" " + word; 
+				//}
+				words.push(word);
     			});
-			return formattedString; 
+			//return formattedString; 
 		}else{
 			alert("Error: word cloud retrieve failed, possibly no words"); 
 		}
@@ -39,11 +46,7 @@ window.onload = function(argument) {
 
 
 
-	var wordsRetrieved = fetchWords(); 
-	//var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
-	var words = {};
-	var words_attr = [];
-	string_handle(wordsRetrieved);
+	
 
 	var canvas = document.getElementById('c');
 	canvas.width = window.innerWidth;
@@ -101,7 +104,7 @@ window.onload = function(argument) {
 
 	function string_handle(str) {
     var lamda = 1;
-		var word_array = str.split(" ");
+		//var word_array = str.split(" ");
     // Give each word a random occurance counrt
 		for (var i = 0; i < word_array.length; i++) {
       var u = Math.random();
