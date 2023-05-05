7df71cd14b8a389b13fb50f3b0d43c2a34e3d85a
michpolak <124084861+michpolak@users.noreply.github.com>
2023-05-04 20:16:13 -0400

    moving functions within canvas

--- words.js
+++ words.js
@@ -70,58 +70,49 @@ window.onload = function(argument) {
 		c.lineWidth = 5;
 
 		fetchWords(); 
-	console.log(wordsRetrieved);
-	//var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
-	//var words = [];
-	//var words_attr = [];
-	//string_handle(wordsRetrieved);
+		console.log(wordsRetrieved);
+		//var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
+		//var words = [];
+		//var words_attr = [];
+		//string_handle(wordsRetrieved);
 	
 	
-	function fetchWords() {
-		// default timestamp, change later
-		timestamp = "2023-05-01";
-   		fetch(baseUrl + '/retrieve/words/'+timestamp, {
-   		method: 'get'
-   		})
-   		.then(response => response.json())
-   		.then(data => formatWords(data))
-   		.catch(error => {
-   	 	{alert("Error: Something went wrong:" + error); }
-  	 	})
+		function fetchWords() {
+			// default timestamp, change later
+			timestamp = "2023-05-01";
+   			fetch(baseUrl + '/retrieve/words/'+timestamp, {
+   			method: 'get'
+   			})
+   			.then(response => response.json())
+   			.then(data => formatWords(data))
+   			.catch(error => {
+   	 		{alert("Error: Something went wrong:" + error); }
+  	 		})
 
-   	}
+   		}
 	
-   	function formatWords(data){
-		var result = data;
-		var formattedString = "";
-		var first = true; 
-	 	if (result["status"]=="success"){
-			var wordList = result["words"]; 
-			wordList.forEach(function (word) {
-				if (first){
-					formattedString+=word; 
-					first = false; 
-				}else{
-					formattedString+=" " + word; 
-				}
-				//words.push(word);
-    			});
-			console.log(formattedString); 
-			wordsRetrieved = formattedString; 
-			string_handle(wordsRetrieved)
-		}else{
-			alert("Error: word cloud retrieve failed, possibly no words"); 
+		function formatWords(data){
+			var result = data;
+			var formattedString = "";
+			var first = true; 
+			if (result["status"]=="success"){
+				var wordList = result["words"]; 
+				wordList.forEach(function (word) {
+					if (first){
+						formattedString+=word; 
+						first = false; 
+					}else{
+						formattedString+=" " + word; 
+					}
+					//words.push(word);
+				});
+				console.log(formattedString); 
+				wordsRetrieved = formattedString; 
+				string_handle(wordsRetrieved)
+			}else{
+				alert("Error: word cloud retrieve failed, possibly no words"); 
+			}
 		}
-	}
-		
-
-
-
-		
-		
-		
-		
-		
 		
 		
 		
