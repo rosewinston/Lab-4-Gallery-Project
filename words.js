640c8727e9b8f056f27de96d0405a6a05f008db8
michpolak <124084861+michpolak@users.noreply.github.com>
2023-05-05 16:13:08 -0400

    cleaning file

--- words.js
+++ words.js
@@ -1,5 +1,5 @@
 window.onload = function(argument) {
-	var baseUrl = 'http://18.222.189.198:5005';
+	var baseUrl = 'http://54.234.252.3:5005';
 	var wordsRetrieved = "";
 	var words = {};
 	var words_attr = [];
@@ -14,15 +14,12 @@ window.onload = function(argument) {
 			h = canvas.height;
 
 		c.strokeStyle = 'red';
-		//c.fillStyle = 'white';
 		c.lineWidth = 5;
 
 		fetchWords(); 
 		console.log(wordsRetrieved);
-		var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
-		//var words = [];
-		//var words_attr = [];
-		//string_handle(wordsRetrieved);
+		
+
 	
 		console.log(words);
 		function fetchWords() {
@@ -60,23 +57,23 @@ window.onload = function(argument) {
 				console.log(typeof wordsRetrieved);
 				string_handle(wordsRetrieved);
 			}else{
-				alert("Error: word cloud retrieve failed, possibly no words"); 
+				alert("Error: Word Cloud Retrieve Failed"); 
 			}
 		}
 		
-		console.log("why is this not running");
-		
+		//function to randomly assign a color to a given word 
 		function colorAssign(){
+			//color options from IBM Design Colorblind Friendly Palette
 			var colorArray = ['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000'];
 			var colorRand = colorArray[Math.floor(Math.random()* 5)]; 
 			return colorRand; 
 		}
 		
 		
-		for (key in words) {	
-			console.log("attempted push");
-			words_attr.push(new Word(key));
-		}
+		//for (key in words) {	
+			//words_attr.push(new Word(key));
+		//}
+		
 		// constructor
 		Word = function(key) {
 			this.text = key;
@@ -86,17 +83,11 @@ window.onload = function(argument) {
 			this.speed = (words[key]);
 			this.color = colorAssign();
 		}
-		console.log("is it running here?");
-		console.log(typeof words);
 		
-		//for (key in words) {	
-			//console.log("attempted push");
-		//	words_attr.push(new Word(key));
-		//}
-		
+		//check to see if words are being constructed and passed successfully
 		console.log(words_attr.length);
 		console.log(words);
-		console.log("it's running here!");
+
 		function animation() {
 			for (var i = 0; i < words_attr.length; i++) {
 				c.font = words_attr[i].font;
@@ -130,21 +121,18 @@ window.onload = function(argument) {
     var lamda = 1;
 		var word_array = str.split(" ");
 		console.log(typeof word_array);
-		//var word_array = str;
+		
     // Give each word a random occurance counrt
 		for (var i = 0; i < word_array.length; i++) {
       var u = Math.random();
       occurance =  -Math.log(u)/(lamda);
       // Should set. this to the number of occurances of word
 			words[word_array[i]] = occurance;
-      console.log(occurance);
 		}
-		console.log(typeof words);
+	
 		for (key in words) {	
-			console.log("attempted push");
 			words_attr.push(new Word(key));
 		}
-		
 		
 		return words;
 	}
