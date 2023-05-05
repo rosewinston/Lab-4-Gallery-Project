3c418bdb33609be2aced1397b884e91bda7eabe4
Ubuntu <ubuntu@ip-172-31-92-181.ec2.internal>
2023-05-05 02:11:24 +0000

    Major Major Edits to Fix, but it's fixed

--- words.js
+++ words.js
@@ -1,61 +1,9 @@
 window.onload = function(argument) {
-	var baseUrl = 'http://18.222.189.198:5005';
+	var baseUrl = 'http://54.234.252.3:5005';
 	var wordsRetrieved = "";
-	var words = [];
+	var words = {};
 	var words_attr = [];
 	
-
-// 	fetchWords(); 
-// 	console.log(wordsRetrieved);
-// 	//var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
-// 	//var words = [];
-// 	//var words_attr = [];
-// 	//string_handle(wordsRetrieved);
-	
-	
-// 	function fetchWords() {
-// 		// default timestamp, change later
-// 		timestamp = "2023-05-01";
-//    		fetch(baseUrl + '/retrieve/words/'+timestamp, {
-//    		method: 'get'
-//    		})
-//    		.then(response => response.json())
-//    		.then(data => formatWords(data))
-//    		.catch(error => {
-//    	 	{alert("Error: Something went wrong:" + error); }
-//   	 	})
-
-//    	}
-	
-//    	function formatWords(data){
-// 		var result = data;
-// 		var formattedString = "";
-// 		var first = true; 
-// 	 	if (result["status"]=="success"){
-// 			var wordList = result["words"]; 
-// 			wordList.forEach(function (word) {
-// 				if (first){
-// 					formattedString+=word; 
-// 					first = false; 
-// 				}else{
-// 					formattedString+=" " + word; 
-// 				}
-// 				//words.push(word);
-//     			});
-// 			console.log(formattedString); 
-// 			wordsRetrieved = formattedString; 
-// 			string_handle(wordsRetrieved)
-// 		}else{
-// 			alert("Error: word cloud retrieve failed, possibly no words"); 
-// 		}
-// 	}
-		
-
-
-
-
-	
-
 	var canvas = document.getElementById('c');
 	canvas.width = window.innerWidth;
 	canvas.height = window.innerHeight;
@@ -66,20 +14,20 @@ window.onload = function(argument) {
 			h = canvas.height;
 
 		c.strokeStyle = 'red';
-		c.fillStyle = 'white';
+		//c.fillStyle = 'white';
 		c.lineWidth = 5;
 
 		fetchWords(); 
 		console.log(wordsRetrieved);
-		//var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
+		var moby = "anger a fear sadness disgust enjoyment happiness love relief contentment amusement joy pride excitement peace satisfaction lonely heartbroken gloomy disappointed hopeless grieved unhappy lost troubled resigned miserable worried doubtful nervous anxious terrified panicked horrified desperate confused stressed annoyed frustrated peeved contrary bitter infuriated irritated mad cheated vengeful insulted dislike revulsion loathing disapproving offended horrified uncomfortable nauseated disturbed withdrawn aversion";
 		//var words = [];
 		//var words_attr = [];
 		//string_handle(wordsRetrieved);
 	
-	
+		console.log(words);
 		function fetchWords() {
 			// default timestamp, change later
-			timestamp = "2023-05-01";
+			timestamp = "2023-05-05";
    			fetch(baseUrl + '/retrieve/words/'+timestamp, {
    			method: 'get'
    			})
@@ -90,32 +38,45 @@ window.onload = function(argument) {
   	 		})
 
    		}
-	
+		console.log(words);
 		function formatWords(data){
 			var result = data;
 			var formattedString = "";
 			var first = true; 
 			if (result["status"]=="success"){
 				var wordList = result["words"]; 
-				wordList.forEach(function (word) {
+				wordList.forEach(function (wordElement) {
 					if (first){
-						formattedString+=word; 
+						formattedString+=wordElement; 
 						first = false; 
 					}else{
-						formattedString+=" " + word; 
+						formattedString+=" " + wordElement; 
 					}
-					//words.push(word);
+					
 				});
-				console.log(formattedString); 
+				console.log(typeof formattedString); 
+				console.log(typeof moby);
 				wordsRetrieved = formattedString; 
-				string_handle(wordsRetrieved)
+				console.log(typeof wordsRetrieved);
+				string_handle(wordsRetrieved);
 			}else{
 				alert("Error: word cloud retrieve failed, possibly no words"); 
 			}
 		}
 		
+		console.log("why is this not running");
+		
+		function colorAssign(){
+			var colorArray = ['#648FFF', '#785EF0', '#DC267F', '#FE6100', '#FFB000'];
+			var colorRand = colorArray[Math.floor(Math.random()* 5)]; 
+			return colorRand; 
+		}
 		
 		
+		for (key in words) {	
+			console.log("attempted push");
+			words_attr.push(new Word(key));
+		}
 		// constructor
 		Word = function(key) {
 			this.text = key;
@@ -123,18 +84,25 @@ window.onload = function(argument) {
 			this.y = Math.random() * h;
 			this.font = words[key] * 50 + 'px arial'
 			this.speed = (words[key]);
+			this.color = colorAssign();
 		}
-		for (key in words) {
-			words_attr.push(new Word(key));
-		}
+		console.log("is it running here?");
+		console.log(typeof words);
+		
+		//for (key in words) {	
+			//console.log("attempted push");
+		//	words_attr.push(new Word(key));
+		//}
+		
 		console.log(words_attr.length);
 		console.log(words);
-
+		console.log("it's running here!");
 		function animation() {
 			for (var i = 0; i < words_attr.length; i++) {
 				c.font = words_attr[i].font;
 				c.fillText(words_attr[i].text, words_attr[i].x, words_attr[i].y);
 				words_attr[i].width = c.measureText(words_attr[i].text).width;
+				c.fillStyle = words_attri[i].color; 
 				c.stroke();
 			}
 			move();
@@ -157,21 +125,28 @@ window.onload = function(argument) {
 		},24);
 
 	}
-
+	console.log(words);
 	function string_handle(str) {
     var lamda = 1;
 		var word_array = str.split(" ");
+		console.log(typeof word_array);
 		//var word_array = str;
     // Give each word a random occurance counrt
-		for (var i = 0; i < str.length; i++) {
+		for (var i = 0; i < word_array.length; i++) {
       var u = Math.random();
       occurance =  -Math.log(u)/(lamda);
       // Should set. this to the number of occurances of word
 			words[word_array[i]] = occurance;
       console.log(occurance);
 		}
-		console.log(words);
-		//return words;
+		console.log(typeof words);
+		for (key in words) {	
+			console.log("attempted push");
+			words_attr.push(new Word(key));
+		}
+		
+		
+		return words;
 	}
 
 }
